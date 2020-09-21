eac549053407c4972c492845f283e45ffadd299c
Smile <4213509@qq.com>
2020-09-21 16:51:28 +0800

    增加浏览器标题设置

--- src/Entity/UISettingEntity.php
+++ src/Entity/UISettingEntity.php
@@ -1,27 +1,39 @@
 <?php
+
 declare(strict_types=1);
+/**
+ * This file is part of Hyperf.plus
+ *
+ * @link     https://www.hyperf.plus
+ * @document https://doc.hyperf.plus
+ * @contact  4213509@qq.com
+ * @license  https://github.com/hyperf/hyperf-plus/blob/master/LICENSE
+ */
 
 namespace HPlus\UI\Entity;
 
 class UISettingEntity extends EntityBean
 {
     /**
-     * 本地模式
+     * 本地模式.
      * @var bool
      */
     public $isLocal = false;
+
     /**
-     * 显示logo
+     * 显示logo.
      * @var bool
      */
     public $logoShow = true;
+
     /**
      * Logo地址
      * @var string
      */
     public $logo = '';
+
     /**
-     * logo高度
+     * logo高度.
      * @var string
      */
     public $logoLight = '';
@@ -31,211 +43,169 @@ class UISettingEntity extends EntityBean
      * @var string
      */
     public $logoMini = '';
+
     /**
-     * 小logo高度
+     * 小logo高度.
      * @var string
      */
     public $logoMiniLight = '';
+
     /**
-     * 站点名称
+     * 站点名称.
      * @var string
      */
     public $name = '';
+
     /**
-     * 版权信息
+     * 浏览器title.
      * @var string
      */
-    public $copyright = "Copyright © 2020 HPlus";
+    public $title = 'HPlus Admin 后台管理系统';
+
     /**
-     * 底部链接
+     * 版权信息.
+     * @var string
+     */
+    public $copyright = 'Copyright © 2020 HPlus';
+
+    /**
+     * 底部链接.
      * @var array
      */
     public $footerLinks = [];
+
     /**
-     * 多标签
+     * 多标签.
      * @var bool
      */
     public $uniqueOpened = false;
+
     /**
-     * 当前登录用户信息
+     * 当前登录用户信息.
      * @var UserEntity
      */
     public $user = [];
+
     /**
-     * 菜单数据
+     * 菜单数据.
      * @var MenuEntity
      */
     public $menu = [];
+
     /**
-     * 菜单数据
+     * 菜单数据.
      * @var array
      */
     public $menuList = [];
+
     /**
-     * 头部URL链接列表
+     * 头部URL链接列表.
      * @var array[]
      */
     public $url = [];
 
     /**
-     * 默认首页
+     * 默认首页.
      * @var string
      */
     public $homeUrl = 'index';
+
     /**
      * API根地址
      * @var string
      */
     public $apiRoot = '';
 
-    /**
-     * @return bool
-     */
     public function isLocal(): bool
     {
         return $this->isLocal;
     }
 
-    /**
-     * @param bool $isLocal
-     */
     public function setIsLocal(bool $isLocal): void
     {
         $this->isLocal = $isLocal;
     }
 
-    /**
-     * @return bool
-     */
     public function isLogoShow(): bool
     {
         return $this->logoShow;
     }
 
-    /**
-     * @param bool $logoShow
-     */
     public function setLogoShow(bool $logoShow): void
     {
         $this->logoShow = $logoShow;
     }
 
-    /**
-     * @return string
-     */
     public function getLogo(): string
     {
         return $this->logo;
     }
 
-    /**
-     * @param string $logo
-     */
     public function setLogo(string $logo): void
     {
         $this->logo = $logo;
     }
 
-    /**
-     * @return string
-     */
     public function getLogoLight(): string
     {
         return $this->logoLight;
     }
 
-    /**
-     * @param string $logoLight
-     */
     public function setLogoLight(string $logoLight): void
     {
         $this->logoLight = $logoLight;
     }
 
-    /**
-     * @return string
-     */
     public function getLogoMini(): string
     {
         return $this->logoMini;
     }
 
-    /**
-     * @param string $logoMini
-     */
     public function setLogoMini(string $logoMini): void
     {
         $this->logoMini = $logoMini;
     }
 
-    /**
-     * @return string
-     */
     public function getLogoMiniLight(): string
     {
         return $this->logoMiniLight;
     }
 
-    /**
-     * @param string $logoMiniLight
-     */
     public function setLogoMiniLight(string $logoMiniLight): void
     {
         $this->logoMiniLight = $logoMiniLight;
     }
 
-    /**
-     * @return string
-     */
     public function getName(): string
     {
         return $this->name;
     }
 
-    /**
-     * @param string $name
-     */
     public function setName(string $name): void
     {
         $this->name = $name;
     }
 
-    /**
-     * @return string
-     */
     public function getCopyright(): string
     {
         return $this->copyright;
     }
 
-    /**
-     * @param string $copyright
-     */
     public function setCopyright(string $copyright): void
     {
         $this->copyright = $copyright;
     }
 
-    /**
-     * @return array
-     */
     public function getFooterLinks(): array
     {
         return $this->footerLinks;
     }
 
-    /**
-     * @param array $footerLinks
-     */
     public function setFooterLinks(array $footerLinks): void
     {
         $this->footerLinks = $footerLinks;
     }
 
-    /**
-     * @param UserEntity $user
-     * @return UISettingEntity
-     */
     public function setUser(UserEntity $user): UISettingEntity
     {
         $this->user = $user;
@@ -250,86 +220,65 @@ class UISettingEntity extends EntityBean
         return $this->menu;
     }
 
-    /**
-     * @param MenuEntity $menu
-     */
     public function setMenu(MenuEntity $menu): void
     {
         $this->menu = $menu->toArray();
         $this->menuList = $menu->toMenuList();
     }
 
-    /**
-     * @return array
-     */
     public function getMenuList(): array
     {
         return $this->menuList;
     }
 
-    /**
-     * @return bool
-     */
     public function isUniqueOpened(): bool
     {
         return $this->uniqueOpened;
     }
 
-    /**
-     * @param bool $uniqueOpened
-     */
     public function setUniqueOpened(bool $uniqueOpened): void
     {
         $this->uniqueOpened = $uniqueOpened;
     }
 
-    /**
-     * @param array $url
-     * @return UISettingEntity
-     */
     public function setUrl(array $url): UISettingEntity
     {
         $this->url = $url;
         return $this;
     }
 
-    /**
-     * @return string
-     */
     public function getApiRoot(): string
     {
         return $this->apiRoot;
     }
 
-    /**
-     * @param string $apiRoot
-     */
     public function setApiRoot(string $apiRoot): void
     {
         $this->apiRoot = $apiRoot;
     }
 
-    /**
-     * @return UserEntity
-     */
     public function getUser(): UserEntity
     {
         return $this->user;
     }
 
-    /**
-     * @return string
-     */
     public function getHomeUrl(): string
     {
         return $this->homeUrl;
     }
 
-    /**
-     * @param string $homeUrl
-     */
     public function setHomeUrl(string $homeUrl): void
     {
         $this->homeUrl = $homeUrl;
+    }
+
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): void
+    {
+        $this->title = $title;
     }
 }
