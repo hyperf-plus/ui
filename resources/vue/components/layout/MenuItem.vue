72f6366d88c422599cd2e11a73c4f68d9ea0a20f
Smile <4213509@qq.com>
2020-12-18 19:05:57 +0800

    更新图标选择器组件，支持更多图标

--- resources/vue/components/layout/MenuItem.vue
+++ resources/vue/components/layout/MenuItem.vue
@@ -1,24 +1,24 @@
 <template>
-    <el-submenu
+  <el-submenu
       :index="menu.route"
       v-if="menu.children && menu.children.length > 0"
-    >
-      <template slot="title">
-        <i :class="menu.icon" v-if="menu.icon" size="16"></i>
-        <span slot="title">{{ menu.title }}</span>
-      </template>
-      <template v-for="children in menu.children">
-        <MenuItem
+  >
+    <template slot="title">
+      <i :class="'iconfont icon' + menu.icon" v-if="menu.icon"></i>
+      <span slot="title">{{ menu.title }}</span>
+    </template>
+    <template v-for="children in menu.children">
+      <MenuItem
           :menu="children"
           :key="children.id"
           :is_collapsed="is_collapsed"
-        />
-      </template>
-    </el-submenu>
-    <el-menu-item :index="menu.route" :route="menu.route" v-else>
-      <i :class="menu.icon" v-if="menu.icon" size="16"></i>
-      <span slot="title">{{ menu.title }}</span>
-    </el-menu-item>
+      />
+    </template>
+  </el-submenu>
+  <el-menu-item :index="menu.route" :route="menu.route" v-else>
+    <i :class="'iconfont icon' + menu.icon" v-if="menu.icon"></i>
+    <span slot="title">{{ menu.title }}</span>
+  </el-menu-item>
 </template>
 <script>
 export default {
